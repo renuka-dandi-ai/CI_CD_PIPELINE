9f6e07c4007822e31890540a416a76f37dc19f8c
renukadandi <renukadandi26@gmail.com>
2026-02-11 12:07:26 +0530

    11th commit

--- backend/main.py
+++ backend/main.py
@@ -3,11 +3,19 @@ from pathlib import Path
 import pickle
 import numpy as np
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Linear Regression Model API")
 import os
 MODEL_PATH = os.path.join(os.path.dirname(__file__), 'model.pkl')
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://ci-cd-pipeline-1-paam.onrender.com"],  # Or ["https://ci-cd-pipeline-1-paam.onrender.com"]
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 try:
     with open(MODEL_PATH, "rb") as f:
